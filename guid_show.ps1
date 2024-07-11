a33ecedb6f1303e62b1ff15bf6ccdaf24441b7af
gohan <44894526+uhoogohan@users.noreply.github.com>
2024-07-11 09:53:49 +0900

    クロージャ対応
    クロージャが使えると知って、スクリプトブロック内の引数代わりにすることができた。

---
+++ guid_show.ps1
@@ -0,0 +1,28 @@
+﻿using module ".\menu.psm1"
+
+class Uho : Menu {
+  Uho($x, $y, $ttt) : base($x, $y, "ぬほほ") {
+    $this.body += [Caption]::new(7, 5, $ttt)
+  }
+}
+
+class Pu : Menu {
+  Pu() : base(0, 0, "おツール") {
+    5..30 | %{
+      $x = New-Guid
+      $num = $_
+      $this.body += [MenuItem]::new(7, $_, "{0:000} : $x" -f $_, $this.mumu.GetNewClosure())
+    }
+  }
+
+  [scriptblock]$pu = {
+  }
+
+  [scriptblock]$mumu = {
+    $uho = [Uho]::new(7, $num, $x)
+    $uho.exe()
+  }
+}
+
+$pu = [Pu]::new()
+$pu.exe()
